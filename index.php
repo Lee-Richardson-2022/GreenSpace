c68876861f4d3fc89347a7a6aa21b06dbb48f4a2
Philip Sykes <philipsykes2004@icloud.com>
2023-03-15 17:14:49 +0000

    Deleted All

---
+++ index.php
@@ -0,0 +1,14 @@
+<link rel="stylesheet" href="site.css">
+
+
+<div class="main-container">
+    <h1>My Performance Dashboard</h1>
+    <div style="display: flex; flex-direction: row; padding-bottom: 10px;"">
+        <a href="#" class="button-primary current">Current</a>
+        <a href="#" class="button-primary">Primary</a>
+        <a href="#" class="button-warning">Warning</a>
+        <a href="#" class="button-danger">Danger</a>
+        
+    </div>
+    <a href="#" class="button-secondary">secondary button</a>
+</div>
