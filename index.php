5d6c1d367d7d557ea9d2cd6d2263d89d74a28e45
Philip Sykes <philipsykes2004@icloud.com>
2023-03-17 16:17:13 +0000

    Added timeline selector element, fixed font sizing

--- index.php
+++ index.php
@@ -1,14 +1,14 @@
 <link rel="stylesheet" href="site.css">
 
-
 <div class="main-container">
     <h1>My Performance Dashboard</h1>
     <div style="display: flex; flex-direction: row; padding-bottom: 10px;"">
-        <a href="#" class="button-primary current">Current</a>
-        <a href="#" class="button-primary">Primary</a>
-        <a href="#" class="button-warning">Warning</a>
-        <a href="#" class="button-danger">Danger</a>
-        
+        <a href="#" class="button button-primary current">My Dashboard</a>
+        <a href="#" class="button button-primary">Waterman</a> 
     </div>
-    <a href="#" class="button-secondary">secondary button</a>
+    <a href="#" class="button-secondary">edit widgets</a>
+    <div style="display: flex;">
+        <div class="timeline-button"></div>
+        <div class="timeline-button current"></div>
+    </div>
 </div>
