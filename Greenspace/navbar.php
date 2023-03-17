bfd11951dacef738c4275c2b8b49555bec7b73e0
Lee Richardson <114064832+Lee-Richardson-2022@users.noreply.github.com>
2023-03-17 15:21:43 +0000

    Add files via upload

---
+++ Greenspace/navbar.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>My Website</title>
+    <link rel="stylesheet" type="text/css" href="site.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
+  </head>
+  
+  <body>
+    <div class="navbar">
+      <button class="logout">Logout</button>
+    </div>
+
+    <div class='Greenspace-image'>
+    <a href="/GreenSpace/HomePage.php" style="margin-top: 75px; margin-left: 75px;">
+            <img border="0" alt="User Icon" src="greenspace-logo.png" width="150" height="150">
+        </a>
+    <p>Waterman group</p>
+    </div>
+    <div class="navbar-2">
+        <div class="dropdown">
+            <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton1" data-bs-toggle="dropdown" aria-expanded="false">
+                Dropdown button
+            </button>
+            <ul class="dropdown-menu" aria-labelledby="dropdownMenuButton1">
+                <li><a class="dropdown-item" href="#">Action</a></li>
+                <li><a class="dropdown-item" href="#">Another action</a></li>
+                <li><a class="dropdown-item" href="#">Something else here</a></li>
+            </ul>
+        </div>
+    </div>
+    
+  </body>
+</html>
+
+
+
