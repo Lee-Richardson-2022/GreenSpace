33b3d808718245e5a4e096e9463ea0b8b88af879
Philip Sykes <philipsykes2004@icloud.com>
2023-03-22 15:24:12 +0000

    Created Performance Dashboard

--- assets.php
+++ assets.php
@@ -4,13 +4,9 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Performance Dashboard</title>
+    <title>Assets</title>
     
     <link rel="stylesheet" href="site.css">
-
-    <?php require("navbar.php"); // adding the navbar
-
-    ?>
 </head>
 <body>
     
